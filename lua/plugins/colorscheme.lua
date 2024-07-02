d6ec8eea02216669f50e9e886d2dbf2486cc1ffb
Manprit Tiwari <tiwarimanprit1@gmail.com>
2024-07-02 09:52:02 +0530

    add: catppuccin Mocha coloursheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,12 @@
 return {
-  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+    },
+  },
   -- {
   --   "sainnhe/sonokai",
   --   priority = 1000,
