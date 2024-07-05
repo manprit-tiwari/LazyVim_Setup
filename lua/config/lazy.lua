b41c12315812522e31dfe2c0f5f2b6c3348cb45f
Manprit Tiwari <tiwarimanprit1@gmail.com>
2024-07-05 10:40:45 +0530

    remove: lazyvim extras neogen plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.angular" },
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    -- { import = "lazyvim.plugins.extras.coding.neogen" },
     -- import/override with your plugins
     { import = "plugins" },
   },
