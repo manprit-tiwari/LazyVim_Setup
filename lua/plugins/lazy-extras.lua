8ea35fe70c80dce4b1c31843180dec9b7faf5a8a
Manprit Tiwari <tiwarimanprit1@gmail.com>
2025-02-19 09:33:29 +0530

    add: lazy Extras and colourscheme

---
+++ lua/plugins/lazy-extras.lua
@@ -0,0 +1,24 @@
+return {
+  -- UI
+  { import = "lazyvim.plugins.extras.ui.mini-animate" },
+  { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
+
+  -- LSP
+  { import = "lazyvim.plugins.extras.lsp.none-ls" },
+
+  -- Lang Extras
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.angular" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.git" },
+  { import = "lazyvim.plugins.extras.lang.sql" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+
+  -- Linter
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+
+  -- Editor
+  { import = "lazyvim.plugins.extras.editor.inc-rename" },
+}
