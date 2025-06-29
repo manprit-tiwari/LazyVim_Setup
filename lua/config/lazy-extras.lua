32e10b878475766f9ef12a036d0efe8396f93591
Manprit Tiwari <tiwarimanprit1@gmail.com>
2025-06-29 18:16:43 +0530

    add: copilot, emmet, and prettier support

--- lua/config/lazy-extras.lua
+++ lua/config/lazy-extras.lua
@@ -2,6 +2,14 @@ return {
   -- UI
   { import = "lazyvim.plugins.extras.ui.mini-animate" },
   { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
+  { import = "lazyvim.plugins.extras.ui.smear-cursor" },
+
+  -- CODING
+  { import = "lazyvim.plugins.extras.coding.blink" },
+
+  -- AI
+  { import = "lazyvim.plugins.extras.ai.copilot" },
+  { import = "lazyvim.plugins.extras.ai.copilot-chat" },
 
   -- LSP
   { import = "lazyvim.plugins.extras.lsp.none-ls" },
@@ -15,9 +23,13 @@ return {
   { import = "lazyvim.plugins.extras.lang.git" },
   { import = "lazyvim.plugins.extras.lang.sql" },
   { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
 
   -- Linter
   { import = "lazyvim.plugins.extras.linting.eslint" },
+
+  -- Formatter
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
 
   -- Editor
   { import = "lazyvim.plugins.extras.editor.inc-rename" },
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,4 +3,8 @@
 -- Add any additional options here
 
 -- Set to false to disable auto format
-vim.g.lazyvim_eslint_auto_format = true
+vim.g.lazyvim_eslint_auto_format = false
+
+-- Enable the option to require a Prettier config file
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = false
